84eb35f651de3c9e25f0adb9f1466191e3851d19
Shubhanshu Arya <shubhanshuarya@Shubhanshus-MacBook-Air.local>
2023-09-03 09:58:28 +0530

    Resolved the new user issue

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,12 +6,13 @@ import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 
 export default async function Home() {
-  const result = await fetchPosts(1, 30);
   const user = await currentUser();
   if(!user) return null;
-
+  
   const userInfo = await fetchUser(user.id);
   if(!userInfo?.onboarded) redirect('/onboarding');
+
+  const result = await fetchPosts(1, 30);
 
   return (
     <>
